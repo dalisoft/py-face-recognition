72063dd3b70ddc2d5d0274ea100f9f545172eca0
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2025-03-11 20:41:59 +0500

    fix: use only matched name, not unknown

--- app.py
+++ app.py
@@ -44,11 +44,10 @@ while True:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        face_names = []
+        face_names: list[str] = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
@@ -59,9 +58,8 @@ while True:
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+                face_names.append(known_face_names[best_match_index])
 
-            face_names.append(name)
 
 
         # Display the results
